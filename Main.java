d93b0eb6a98bb9c9b3db7123f8c228664a65517b
tomas.helmfridsson <tomas.helmfridsson@sj.se>
2025-01-30 08:24:04 +0100

    Main filen klar

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+
+    // file unstage  add för att få versionshistorik -> stage
+    // commit för att skapa ett paket som skall skickas till github
+
+}
