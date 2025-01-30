b8ff9a5016b4c47db239da46a516410cef613b67
tomas.helmfridsson <tomas.helmfridsson@sj.se>
2025-01-30 08:32:50 +0100

    Ändå mer kommentarer

--- Main.java
+++ Main.java
@@ -6,5 +6,11 @@ public class Main {
     // file unstage  add för att få versionshistorik -> stage
     // commit för att skapa ett paket som skall skickas till github
     // Push för att skicka till git
+    // definerar en git origin genom att skapa en repository i github (webben)
+    // kopierar länken.
+    // i "Push" fönstret sätter vi origin url'en
+    // Sedan kan vi pusha.
+
+    // Därefter är det bara commit och push på allt som skall upp
 
 }
